fa1dc216e024083cf9d3cacb040822b2f97d7596
saiteja <saitejachicky@gmail.com>
2024-06-27 07:31:25 +0530

    checking conflicts

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
         <h1>this is a main branch</h1>
+        <h2>this is also a main branch to check conflicts</h2>
       </header>
     
     </div>
